9a42f93c043c1a479b91e5cc6fc2237d3fd5b262
Low Kian Seong <kianseong@gmail.com>
2014-09-22 12:06:58 +0800

    Adding description to setup.py and removing pep8 violations

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from distutils.core import setup
 
 setup(
-  name = 'postgresqlbackup',
-  url = 'https://github.com/Version2beta/postgresqlbackup',
-  version = '1.0.5',
-  author = 'Rob Martin',
-  author_email = 'rob@version2beta.com',
-  packages = ['postgresqlbackup'],
-  install_requires = [],
-  data_files = [('/etc/postgresql', ['backups.conf'])],
-  entry_points = {
-    'console_scripts': [
-      'postgresql_backup = postgresqlbackup.postgresqlbackup:main'
-    ]
-  }
+    name='postgresqlbackup',
+    url='https://github.com/Version2beta/postgresqlbackup',
+    version='1.0.5',
+    author='Rob Martin',
+    author_email='rob@version2beta.com',
+    description=('Back up all PostgreSQL databases to a local '
+                 'directory plus rotating hourly and seven day on Amazon S3.'),
+    packages=['postgresqlbackup'],
+    install_requires=[],
+    data_files=[('/etc/postgresql', ['backups.conf'])],
+    entry_points={
+        'console_scripts': [
+            'postgresql_backup=postgresqlbackup.postgresqlbackup:main'
+        ]
+    }
 )
